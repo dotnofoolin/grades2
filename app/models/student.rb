38e580dea2d619d71d32556d3d42ce91488ca830
Josh Burks <dotnofoolin@gmail.com>
2017-12-19 09:22:54 -0600

    Order the courses on the dashboard by last_update, and omit any without valid averages.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
   has_many :courses
   has_many :assignments
+
+  def courses_for_dashboard
+    courses.where('average > 0.0').order('last_update DESC')
+  end
 end
