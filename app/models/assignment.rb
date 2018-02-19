711969149f22aec83a5cd52a6ea268a21903f819
Josh Burks <dotnofoolin@gmail.com>
2018-02-19 14:29:30 -0600

    Add due_date_formatted and date_assigned_formatted to Assignment model.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,14 @@
 class Assignment < ApplicationRecord
   belongs_to :student
   belongs_to :course
+
+  def due_date_formatted
+    return nil if due_date.blank?
+    due_date.strftime('%m/%d/%Y')
+  end
+
+  def date_assigned_formatted
+    return nil if date_assigned.blank?
+    date_assigned.strftime('%m/%d/%Y')
+  end
 end
--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Assignment, type: :model do
+  let(:assignment) { Fabricate(:assignment) }
+
+  describe '#due_date_formatted' do
+    subject {
+      assignment.due_date_formatted
+    }
+
+    it { expect(subject).to be_an_instance_of(String) }
+  end
+
+  describe '#date_assigned_formatted' do
+    subject {
+      assignment.date_assigned_formatted
+    }
+
+    it { expect(subject).to be_an_instance_of(String) }
+  end
 end
