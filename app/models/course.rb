0959496880d4f93b786acc77bdcebe2427fa0390
Josh Burks <dotnofoolin@gmail.com>
2019-02-08 13:49:07 -0600

    Add health_bar_color_class method to course model. Add tests.

--- app/models/course.rb
+++ app/models/course.rb
@@ -12,4 +12,12 @@ class Course < ApplicationRecord
     return nil if last_update.blank?
     last_update.strftime('%m/%d/%Y')
   end
+
+  def health_bar_color_class
+    return 'is-success' if grade_letter(average) == 'A'
+    return 'is-primary' if grade_letter(average) == 'B'
+    return 'is-info' if grade_letter(average) == 'C'
+    return 'is-warning' if grade_letter(average) == 'D'
+    'is-danger'
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -56,4 +56,50 @@ RSpec.describe Course, type: :model do
 
     it { expect(subject).to be_an_instance_of(String) }
   end
+
+  describe '#health_bar_color_class' do
+    subject {
+      course.health_bar_color_class
+    }
+
+    describe 'when grade is an A' do
+      before do
+        course.update(average: 95.00)
+      end
+
+      it { expect(subject).to eq('is-success') }
+    end
+
+    describe 'when grade is a B' do
+      before do
+        course.update(average: 85.00)
+      end
+
+      it { expect(subject).to eq('is-primary') }
+    end
+
+    describe 'when grade is a C' do
+      before do
+        course.update(average: 75.00)
+      end
+
+      it { expect(subject).to eq('is-info') }
+    end
+
+    describe 'when grade is a D' do
+      before do
+        course.update(average: 65.00)
+      end
+
+      it { expect(subject).to eq('is-warning') }
+    end
+
+    describe 'when grade is a F' do
+      before do
+        course.update(average: 55.00)
+      end
+
+      it { expect(subject).to eq('is-danger') }
+    end
+  end
 end
